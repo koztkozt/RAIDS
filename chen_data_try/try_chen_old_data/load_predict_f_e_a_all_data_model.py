8e21104e900553c112ed4eb2b705caabeed9daa8
kozt <ko.zt100@gmail.com>
2022-05-08 07:23:28 +0000

    response time

--- chen_data_try/try_chen_old_data/load_predict_f_e_a_all_data_model.py
+++ chen_data_try/try_chen_old_data/load_predict_f_e_a_all_data_model.py
@@ -1,9 +1,9 @@
-from __future__ import print_function, division
+# from __future__ import print_function, division
+from sklearn.model_selection import train_test_split
 import os
 import torch
 import pandas as pd
 from skimage import io, transform
-import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
@@ -11,6 +11,7 @@ from PIL import Image
 import cv2
 import torch.optim as optim
 from data_extraction_ABS import *
+from torch.autograd import Variable
 
 from torch.optim import lr_scheduler
 from torch.autograd import Variable
@@ -29,22 +30,19 @@ from collections import deque
 from keras.models import load_model
 from keras.preprocessing.image import load_img, img_to_array
 from skimage.exposure import rescale_intensity
-from train import *
+
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score
 
 import datetime
 
 time_a = datetime.datetime.now()
-
 print("intrusion_detection_start: ", time_a)
 
 
 class Model(object):
-    def __init__(self,
-                 model_path,
-                 X_train_mean_path):
+    def __init__(self, model_path, X_train_mean_path):
 
         self.model = model_path
-        # self.model = load_model(model_path)
         self.X_mean = np.load(X_train_mean_path)
         self.mean_angle = np.array([-0.004179079])
         self.img0 = None
@@ -56,8 +54,6 @@ class Model(object):
 
         if self.img0 is None:
             self.img0 = img1
-
-            # print("00000000")
             return self.mean_angle
 
         elif len(self.state) < 1:
@@ -66,14 +62,13 @@ class Model(object):
             img = np.array(img, dtype=np.uint8)  # to replicate initial model
             self.state.append(img)
             self.img0 = img1
+
             return self.mean_angle
 
         else:
             img = img1 - self.img0
             img = rescale_intensity(img, in_range=(-255, 255), out_range=(0, 255))
-
             img = np.array(img, dtype=np.uint8)  # to replicate initial model
-
             self.state.append(img)
             self.img0 = img1
 
@@ -81,176 +76,148 @@ class Model(object):
             X = X[:, :, ::-1]
 
             X = np.expand_dims(X, axis=0)
-            X = X.astype('float32')
+            X = X.astype("float32")
             X -= self.X_mean
             X /= 255.0
 
             return self.model.predict(X)[0]
 
 
-test_dataset=intrusion_data(csv_file='interpolated_random_a_10702to24371_part1_0p2to0p9_4103.csv',root_dir='/home/pi/new_try_raspberry_pi--/chen_data_try/chen_old_try/chen_old_data',cs=1,
-                            transform=transforms.Compose([transforms.Resize(256),transforms.RandomResizedCrop(224),transforms.ToTensor()]))
+if __name__ == "__main__":
 
-test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
+    # face_dataset = intrusion_data(csv_file='interpolated_random_a_10702to24371_part1_0p2to0p9_9569.csv',root_dir='/home/pi/new_try_raspberry_pi--/chen_data_try/chen_old_try/chen_old_data',
+    #                               cs=1,transform=transforms.Compose([transforms.Resize(256),transforms.RandomResizedCrop(224),transforms.ToTensor()]))
+    # test_dataset=intrusion_data(csv_file='interpolated_random_a_10702to24371_part1_0p2to0p9_4103.csv',root_dir='/home/pi/new_try_raspberry_pi--/chen_data_try/chen_old_try/chen_old_data',cs=1,
+    #                             transform=transforms.Compose([transforms.Resize(256),transforms.RandomResizedCrop(224),transforms.ToTensor()]))
+    # importing the dataset csv with attacks
+    # dataset = pd.read_csv(
+    #     "/home/ubuntu/RAIDS/chen_data_try/try_chen_old_data/attack_csv/chen_old_all_abrupt_intrusion.csv"
+    # )
+    dataset = pd.read_csv(
+        "/home/ubuntu/RAIDS/chen_data_try/try_chen_old_data/attack_csv/chen_old_all_directed_intrusion.csv"
+    )
+    # split the dataset into training and test. needs to be in order cos the images are substracted
+    face_dataset, test_dataset = train_test_split(dataset, test_size=0.3, shuffle=False, random_state=56)
+    face_dataset = intrusion_data(
+        face_dataset,
+        root_dir="/home/ubuntu/RAIDS/dataset/chen_old/data",
+        transform=transforms.Compose(
+            [transforms.Resize(256), transforms.RandomResizedCrop(224), transforms.ToTensor()]
+        ),
+    )
+    test_dataset = intrusion_data(
+        test_dataset,
+        root_dir="/home/ubuntu/RAIDS/dataset/chen_old/data",
+        transform=transforms.Compose(
+            [transforms.Resize(256), transforms.RandomResizedCrop(224), transforms.ToTensor()]
+        ),
+    )
 
-config = TestConfig()
+    # It represents a Python iterable over a datase
+    dataloader = DataLoader(face_dataset, batch_size=1, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
 
-ch = config.num_channels
-row = config.img_height
-col = config.img_width
-model_org = create_comma_model_large_dropout(row, col, ch, load_weights=True)
+    # see config.py
+    config = TestConfig()
+    ch = config.num_channels
+    row = config.img_height
+    col = config.img_width
 
-model = Model(model_org,
-              "data/X_train_gray_diff2_mean.npy")
-net = mynet()
+    # import CNN Model for phase 1 to extract road context
+    model_org = create_comma_model_large_dropout(
+        row,
+        col,
+        ch,
+        load_weights=True,
+        path="/home/ubuntu/RAIDS/chen_data_try/feature_extract_2cnn/Model_0.0137loss.h5",
+    )
+    model = Model(model_org, "/home/ubuntu/RAIDS/chen_data_try/feature_extract_2cnn/data/X_train_gray_diff2_mean.npy")
 
-criterion = F.nll_loss
+    # Neural networks can be constructed using the torch.nn package.
+    net = mynet()
+    criterion = F.nll_loss  # The negative log likelihood loss.
+    optimizer = optim.Adam(net.parameters(), lr=0.01)  # Implements Adam algorithm.
 
-optimizer = optim.Adam(net.parameters(), lr=0.01)
+    net.eval()
+    net = torch.load("saved_consecutive_models/epoch_39.pt")
 
-def accuracy(act , out):
-    wrong1=0
-    for a,f in zip(act,out):
-        if a!=f:
-            wrong1+=1
-    return wrong1
+    df_all = pd.DataFrame(columns=["TN", "FP", "FN", "TP", "Accuracy", "Precision", "Recall"])
+    y_pred = []
+    y_true = []
 
+    print("Length of dataloader :", len(test_loader))
+    inter = 0.01
 
-net.eval()
-net = torch.load('saved_consecutive_models/epoch_38.pt')
-loss1 = 0
-finale = 0
-train_loss = 0
-finale_1 = 0
-finale_0 = 0
-get_1 = 0
-get_0 = 0
-target_1_indices_1 = 0
-target_1_indices_0 = 0
-target_0_indices_1 = 0
-target_0_indices_0 = 0
-for i_batch, sample in enumerate(test_loader):
-    ave_time = 0
-    max_time = 0
+    total_time = datetime.timedelta()
+    max_time = datetime.timedelta()
 
-    data = []
-    result = []
-    for sm in sample:
-        imag, dat, res = sm
-        data = dat
-        result.append(res)
+    for i_batch, sample in enumerate(test_loader):
+        if i_batch / len(test_loader) > inter:
+            print("epoch: test", " completed: ", inter * 100, "%")
+            inter += 0.01
 
+        data = []
+        result = []
+        for sm in sample:
+            imag, dat, res = sm
+            data = dat
+            result.append(res)
 
-    for img in imag:
-        # print("load_one_image_start: ", strftime("%Y-%m-%d %H:%M:%S", gmtime()))
-        # time1 = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-        time1 = datetime.datetime.now()
+        for img in imag:
+            time1 = datetime.datetime.now()
 
-        preds = model.predict(img)
+            preds = model.predict(img)
+            preds = preds.astype("float").reshape(-1)
+            preds = preds[0]
 
-        preds = preds.astype('float').reshape(-1)
-        preds = preds[0]
-        # print("preds: ", preds)
-        # time2 = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-        # time2 = datetime.datetime.now()
-        # k = time2 - time1
-        #
-        # print("del_time1-time2: ", k, time1, time2)
-        # print("load_one_image_end: ", strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+        target = torch.stack(result)
+        target = target.view(-1)
+        y_true.extend(target)
 
-    time_start = time1
-    target = torch.stack(result)
-    target = target.view(-1)
-    # final_vars = []
-    # for tens in data:
-    #     final_vars.append(Variable(tens))
+        final_vars = []
+        final_vars = torch.FloatTensor([[[abs(data - preds)]]])
 
-    final_vars = torch.FloatTensor([[[abs(data - preds)]]])
+        # forward + backward + optimize
+        x = net(*final_vars)
+        values, indices = x.max(1)
+        y_pred.extend(indices.data)
 
-    # final_vars = torch.FloatTensor([[[data, preds]]]).cuda()
-    # print("preds_data_ibatch: ", preds, data, i_batch)
+        time3 = datetime.datetime.now()
+        # print("detection_result: ", time3)
+        k = time3 - time1
+        # print("input-output: ", k)
+        # k = int(k)
+        if max_time < k:
+            max_time = k
+        total_time = total_time + k
 
-    x = net(*final_vars)
+    # Build confusion matrix
+    cf_matrix = confusion_matrix(y_true, y_pred).flatten()
+    accuracy = accuracy_score(y_true, y_pred)
+    precision = precision_score(y_true, y_pred)
+    recall = recall_score(y_true, y_pred)
+    print("TN, FP, FN, TP :", cf_matrix)
+    print("accuracy :", accuracy)
 
-    values, indices = x.max(1)
-    # print("target_indices: ", target, indices.data)
-    # time3 = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-    time3 = datetime.datetime.now()
+    # Initialize a blank dataframe and keep adding
+    df = pd.DataFrame(columns=["TN", "FP", "FN", "TP", "Accuracy", "Precision", "Recall"])
+    print(type(cf_matrix))
+    df.loc["time"] = cf_matrix.tolist() + [accuracy, precision, recall]
+    df["Total_Actual_Neg"] = df["TN"] + df["FP"]
+    df["Total_Actual_Pos"] = df["FN"] + df["TP"]
+    df["Total_Pred_Neg"] = df["TN"] + df["FN"]
+    df["Total_Pred_Pos"] = df["FP"] + df["TP"]
+    df["TP_Rate"] = df["TP"] / df["Total_Actual_Pos"]  # Recall
+    df["FP_Rate"] = df["FP"] / df["Total_Actual_Neg"]
+    df["TN_Rate"] = df["TN"] / df["Total_Actual_Neg"]
+    df["FN_Rate"] = df["FN"] / df["Total_Actual_Pos"]
+    df["max_time"] = max_time
+    df["total_time"] = total_time
+    df["avg_time"] = total_time / len(test_loader)
 
-    # print("detection_result: ", time3)
-    k = time3-time_start
-    print("input-output: ", k)
-    # k =int(k)
-    #
-    # if max_time < k :
-    #     max_time = k
-    # ave_time = ave_time +k
+    # df.to_csv("accuracy_file_f_e_predict_abrupt_time.csv")
+    df.to_csv("accuracy_file_f_e_predict_directed_time.csv")
 
-    loss1 += accuracy(target, indices.data)
-    finale += 1
-
-    for Target, Indices in zip(target, indices.data):
-        if Target == 1:
-            finale_1 += 1
-        else:
-            finale_0 += 1
-        if Indices == 1:
-            get_1 += 1
-        else:
-            get_0 += 1
-        if Target == 1 and Indices == 1:
-            target_1_indices_1 += 1
-        elif Target == 1 and Indices == 0:
-            target_1_indices_0 += 1
-        elif Target == 0 and Indices == 0:
-            target_0_indices_0 += 1
-        elif Target == 0 and Indices == 1:
-            target_0_indices_1 += 1
-
-    # if (i_batch > 1000):
-    #     break
-# print("max_time: ", max_time)
-# print("_all_time: ", ave_time)
-
-
-acc = 1.0 - (loss1 / finale)
-acc11 = target_1_indices_1 / (target_1_indices_1 + target_1_indices_0)
-print('accuracy=', acc)
-print('accuracy11=', acc11)
-print("finale_1_0_get_1_0: ", finale_1, finale_0, get_1, get_0)
-print("target_indices11,10,00,01: ", target_1_indices_1, target_1_indices_0, target_0_indices_0, target_0_indices_1)
-
-file2write = open("accuracy_file_f_e_predict_a_random.txt", 'a')
-file2write.write("accuracy= " + str(acc) + "\n")
-file2write.write("accuracy1111= " + str(acc11) + "\n")
-
-file2write.write(
-    "finale_1_0_get_1_0= " + "\n" + str(finale_1) + "\n" + str(finale_0) + "\n" + str(get_1) + "\n" + str(
-        get_0) + "\n")
-file2write.write(
-    "target_indices11,10,00,01= " + "\n" + str(target_1_indices_1) + "\n" + str(target_1_indices_0) + "\n" + str(
-        target_0_indices_0) + "\n" + str(target_0_indices_1) + "\n")
-file2write.close()
-
-time_b = datetime.datetime.now()
-print("intrusion_detection_end: ", time_b)
-print("all_time: ", time_b-time_a)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    time_b = datetime.datetime.now()
+    print("intrusion_detection_end: ", time_b)
+    print("all_time: ", time_b - time_a)
--- udacity_data_try/try_udacity_data_2cnn/load_predict_f_e_a_all_data_model.py
+++ udacity_data_try/try_udacity_data_2cnn/load_predict_f_e_a_all_data_model.py
@@ -1,9 +1,9 @@
-from __future__ import print_function, division
+# from __future__ import print_function, division
+from sklearn.model_selection import train_test_split
 import os
 import torch
 import pandas as pd
 from skimage import io, transform
-import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
@@ -11,12 +11,11 @@ from PIL import Image
 import cv2
 import torch.optim as optim
 from data_extraction_ABS import *
-
-
-from model_abs_linear import *
-from torch.optim import lr_scheduler
 from torch.autograd import Variable
 
+from torch.optim import lr_scheduler
+from torch.autograd import Variable
+from model_abs_linear import *
 
 import numpy as np
 from keras.models import load_model
@@ -32,23 +31,18 @@ from keras.models import load_model
 from keras.preprocessing.image import load_img, img_to_array
 from skimage.exposure import rescale_intensity
 
-
-from train import *
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score
 
 import datetime
 
 time_a = datetime.datetime.now()
-
 print("intrusion_detection_start: ", time_a)
 
 
 class Model(object):
-    def __init__(self,
-                 model_path,
-                 X_train_mean_path):
+    def __init__(self, model_path, X_train_mean_path):
 
         self.model = model_path
-        # self.model = load_model(model_path)
         self.X_mean = np.load(X_train_mean_path)
         self.mean_angle = np.array([-0.004179079])
         self.img0 = None
@@ -60,8 +54,6 @@ class Model(object):
 
         if self.img0 is None:
             self.img0 = img1
-
-            # print("00000000")
             return self.mean_angle
 
         elif len(self.state) < 1:
@@ -71,7 +63,6 @@ class Model(object):
             self.state.append(img)
             self.img0 = img1
 
-            # print("------------------")
             return self.mean_angle
 
         else:
@@ -87,142 +78,141 @@ class Model(object):
             X = X[:, :, ::-1]
 
             X = np.expand_dims(X, axis=0)
-            X = X.astype('float32')
+            X = X.astype("float32")
             X -= self.X_mean
             X /= 255.0
 
             return self.model.predict(X)[0]
 
 
-# face_dataset = intrusion_data(csv_file='interpolated_attack_a_consecutive_0p25_1_1382.csv',root_dir='./HMB_4',cs=1,transform=transforms.Compose([transforms.Resize(256),transforms.RandomResizedCrop(224),transforms.ToTensor()]))
-test_dataset=intrusion_data(csv_file='interpolated_attack_a_directive_0p25_1_all_test.csv',root_dir='/home/pi/new_try_raspberry_pi/udacity_data_try_pi/all',cs=1,transform=transforms.Compose([transforms.Resize(256),transforms.RandomResizedCrop(224),transforms.ToTensor()]))
+if __name__ == "__main__":
+    # face_dataset = intrusion_data(csv_file='interpolated_center_attack_consecutive_0p2to0p9_all_train.csv',
+    #                               root_dir='/home/pi/new_try_raspberry_pi/udacity_data_try_pi/all',
+    #                               cs=1,transform=transforms.Compose([transforms.Resize(256),
+    #                                                                                    transforms.RandomResizedCrop(224),transforms.ToTensor()]))
+    # test_dataset=intrusion_data(csv_file='interpolated_center_attack_consecutive_0p2to0p9_all_test.csv',
+    #                             root_dir='/home/pi/new_try_raspberry_pi/udacity_data_try_pi/all',
+    #                             cs=1,transform=transforms.Compose([transforms.Resize(256),
+    #                                                                                  transforms.RandomResizedCrop(224),transforms.ToTensor()]))
+    # # out=[]
+    # dataloader = DataLoader(face_dataset, batch_size=1, shuffle=False)
+    # test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
 
-test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
+    # dataset = pd.read_csv(
+    #     "/home/ubuntu/RAIDS/udacity_data_try/try_udacity_data_2cnn/attack_csv/udacity_all_abrupt_intrusion.csv"
+    # )
+    dataset = pd.read_csv(
+        "/home/ubuntu/RAIDS/udacity_data_try/try_udacity_data_2cnn/attack_csv/udacity_all_directed_intrusion.csv"
+    )
+    # split the dataset into training and test. needs to be in order cos the images are substracted
+    face_dataset, test_dataset = train_test_split(dataset, test_size=0.3, shuffle=False, random_state=56)
+    face_dataset = intrusion_data(face_dataset, root_dir="/home/ubuntu/RAIDS/dataset/udacity/CH2_002/output")
+    test_dataset = intrusion_data(test_dataset, root_dir="/home/ubuntu/RAIDS/dataset/udacity/CH2_002/output")
 
-config = TestConfig()
+    # It represents a Python iterable over a datase
+    dataloader = DataLoader(face_dataset, batch_size=1, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
 
-ch = config.num_channels
-row = config.img_height
-col = config.img_width
-model_org = create_comma_model_large_dropout(row, col, ch, load_weights=True)
+    config = TestConfig()
 
-model = Model(model_org,
-              "data/X_train_gray_diff2_mean.npy")
-net = mynet()
+    ch = config.num_channels
+    row = config.img_height
+    col = config.img_width
+    model_org = create_comma_model_large_dropout(
+        row,
+        col,
+        ch,
+        load_weights=True,
+        path="/home/ubuntu/RAIDS/udacity_data_try/feature_extraction_100m100_2cnn/Model.h5",
+    )
 
-criterion = F.nll_loss
+    model = Model(model_org, "data/X_train_gray_diff2_mean.npy")
 
-optimizer = optim.Adam(net.parameters(), lr=0.01)
+    # Neural networks can be constructed using the torch.nn package.
+    net = mynet()
+    criterion = F.nll_loss  # The negative log likelihood loss.
+    optimizer = optim.Adam(net.parameters(), lr=0.01)  # Implements Adam algorithm.
 
-def accuracy(act , out):
-    wrong1=0
-    for a,f in zip(act,out):
-        if a!=f:
-            wrong1+=1
-    return wrong1
+    net.eval()
+    net = torch.load("saved_consecutive_models/epoch_39.pt")
 
+    df_all = pd.DataFrame(columns=["TN", "FP", "FN", "TP", "Accuracy", "Precision", "Recall"])
+    y_pred = []
+    y_true = []
 
-net.eval()
-net = torch.load('saved_consecutive_models/epoch_4.pt')
-loss1 = 0
-finale = 0
-train_loss = 0
-finale_1 = 0
-finale_0 = 0
-get_1 = 0
-get_0 = 0
-target_1_indices_1 = 0
-target_1_indices_0 = 0
-target_0_indices_1 = 0
-target_0_indices_0 = 0
-for i_batch, sample in enumerate(test_loader):
-    ave_time = 0
-    max_time = 0
+    print("Length of dataloader :", len(test_loader))
+    inter = 0.01
 
-    data = []
-    result = []
-    for sm in sample:
-        imag, dat, res = sm
-        data = dat
-        result.append(res)
+    total_time = datetime.timedelta()
+    max_time = datetime.timedelta()
 
+    for i_batch, sample in enumerate(test_loader):
+        if i_batch / len(test_loader) > inter:
+            print("epoch: test", " completed: ", inter * 100, "%")
+            inter += 0.01
 
-    for img in imag:
-        time1 = datetime.datetime.now()
-        preds = model.predict(img)
-        preds = preds.astype('float').reshape(-1)
-        preds = preds[0]
-    time_start = time1
-    target = torch.stack(result)
-    target = target.view(-1)
-    final_vars = torch.FloatTensor([[[abs(data - preds)]]])
+        data = []
+        result = []
+        for sm in sample:
+            imag, dat, res = sm
+            data = dat
+            result.append(res)
 
-    x = net(*final_vars)
+        for img in imag:
+            time1 = datetime.datetime.now()
 
-    values, indices = x.max(1)
-    time3 = datetime.datetime.now()
+            preds = model.predict(img)
+            preds = preds.astype("float").reshape(-1)
+            preds = preds[0]
 
-    k = time3-time_start
-    print("input-output: ", k)
-    loss1 += accuracy(target, indices.data)
-    finale += 1
+        target = torch.stack(result)
+        target = target.view(-1)
+        y_true.extend(target)
 
-    for Target, Indices in zip(target, indices.data):
-        if Target == 1:
-            finale_1 += 1
-        else:
-            finale_0 += 1
-        if Indices == 1:
-            get_1 += 1
-        else:
-            get_0 += 1
-        if Target == 1 and Indices == 1:
-            target_1_indices_1 += 1
-        elif Target == 1 and Indices == 0:
-            target_1_indices_0 += 1
-        elif Target == 0 and Indices == 0:
-            target_0_indices_0 += 1
-        elif Target == 0 and Indices == 1:
-            target_0_indices_1 += 1
+        final_vars = []
+        final_vars = torch.FloatTensor([[[abs(data - preds)]]])
 
-acc = 1.0 - (loss1 / finale)
-acc11 = target_1_indices_1 / (target_1_indices_1 + target_1_indices_0)
-print('accuracy=', acc)
-print('accuracy11=', acc11)
-print("finale_1_0_get_1_0: ", finale_1, finale_0, get_1, get_0)
-print("target_indices11,10,00,01: ", target_1_indices_1, target_1_indices_0, target_0_indices_0, target_0_indices_1)
+        # forward + backward + optimize
+        x = net(*final_vars)
+        values, indices = x.max(1)
+        y_pred.extend(indices.data)
 
-file2write = open("accuracy_file_f_e_predict_a_random.txt", 'a')
-file2write.write("accuracy= " + str(acc) + "\n")
-file2write.write("accuracy1111= " + str(acc11) + "\n")
+        time3 = datetime.datetime.now()
+        # print("detection_result: ", time3)
+        k = time3 - time1
+        # print("input-output: ", k)
+        # k = int(k)
+        if max_time < k:
+            max_time = k
+        total_time = total_time + k
 
-file2write.write(
-    "finale_1_0_get_1_0= " + "\n" + str(finale_1) + "\n" + str(finale_0) + "\n" + str(get_1) + "\n" + str(
-        get_0) + "\n")
-file2write.write(
-    "target_indices11,10,00,01= " + "\n" + str(target_1_indices_1) + "\n" + str(target_1_indices_0) + "\n" + str(
-        target_0_indices_0) + "\n" + str(target_0_indices_1) + "\n")
-file2write.close()
+    # Build confusion matrix
+    cf_matrix = confusion_matrix(y_true, y_pred).flatten()
+    accuracy = accuracy_score(y_true, y_pred)
+    precision = precision_score(y_true, y_pred)
+    recall = recall_score(y_true, y_pred)
+    print("TN, FP, FN, TP :", cf_matrix)
+    print("accuracy :", accuracy)
 
-time_b = datetime.datetime.now()
-print("intrusion_detection_end: ", time_b)
-print("all_time: ", time_b-time_a)
+    # Initialize a blank dataframe and keep adding
+    df = pd.DataFrame(columns=["TN", "FP", "FN", "TP", "Accuracy", "Precision", "Recall"])
+    print(type(cf_matrix))
+    df.loc["time"] = cf_matrix.tolist() + [accuracy, precision, recall]
+    df["Total_Actual_Neg"] = df["TN"] + df["FP"]
+    df["Total_Actual_Pos"] = df["FN"] + df["TP"]
+    df["Total_Pred_Neg"] = df["TN"] + df["FN"]
+    df["Total_Pred_Pos"] = df["FP"] + df["TP"]
+    df["TP_Rate"] = df["TP"] / df["Total_Actual_Pos"]  # Recall
+    df["FP_Rate"] = df["FP"] / df["Total_Actual_Neg"]
+    df["TN_Rate"] = df["TN"] / df["Total_Actual_Neg"]
+    df["FN_Rate"] = df["FN"] / df["Total_Actual_Pos"]
+    df["max_time"] = max_time
+    df["total_time"] = total_time
+    df["avg_time"] = total_time / len(test_loader)
 
+    # df.to_csv("accuracy_file_f_e_predict_abrupt_time.csv")
+    df.to_csv("accuracy_file_f_e_predict_directed_time.csv")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    time_b = datetime.datetime.now()
+    print("intrusion_detection_end: ", time_b)
+    print("all_time: ", time_b - time_a)
